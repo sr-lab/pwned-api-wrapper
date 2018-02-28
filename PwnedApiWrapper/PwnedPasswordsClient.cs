d56c9f4b865441f46d8e0c649577906c5ee653c7
Saul Johnson <saul.a.johnson@gmail.com>
2018-02-28 18:20:03 +0000

    Deprecate API call and use new function

--- PwnedApiWrapper/PwnedPasswordsClient.cs
+++ PwnedApiWrapper/PwnedPasswordsClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +26,7 @@ namespace PwnedApiWrapper
         /// <summary>
         /// Initializes a new instance of a Pwned Passwords API client.
         /// </summary>
-        /// <param name="baseUrl"></param>
+        /// <param name="baseUrl">The base URL of the API.</param>
         public PwnedPasswordsClient(string baseUrl)
         {
             BaseUrl = baseUrl + (baseUrl.EndsWith("/") ? "" : "/"); // Append trailing slash if needed.
@@ -33,11 +34,87 @@ namespace PwnedApiWrapper
         }
 
         /// <summary>
-        /// Gets the number of times the given password appears in the 
+        /// Returns the SHA-1 hash of the given password as a hexadecimal string.
+        /// </summary>
+        /// <param name="password">The password to hash.</param>
+        /// <param name="lower">If true, the hash will be returned in lowercase.</param>
+        /// <returns></returns>
+        private string Sha1(string password, bool lower = false)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString((lower ? "x" : "X") + "2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Splits a string by lines.
+        /// </summary>
+        /// <param name="str">The string to split.</param>
+        /// <returns></returns>
+        private string[] SplitByLines(string str)
+        {
+            return str.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hashPrefix"></param>
+        /// <returns></returns>
+        public Dictionary<string, int> GetRange(string hashPrefix)
+        {
+            // Download range.
+            var response = client.DownloadString($"{BaseUrl}/range/{hashPrefix}");
+
+            // Loop through response lines.
+            var lines = SplitByLines(response);
+            var output = new Dictionary<string, int>();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                // Split along colon, add to output if line is valid.
+                var entry = lines[i].Split(':');
+                if (entry.Length == 2 && int.TryParse(entry[1], out int count))
+                {
+                    output.Add(entry[0], count);
+                }
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Gets the number of times the given password appears according to the service.
         /// </summary>
         /// <param name="password">The password to look up.</param>
         /// <returns></returns>
         public int GetNumberOfAppearances(string password)
+        {
+            // Compute hash, prefix and suffix.
+            var hash = Sha1(password);
+            var prefix = hash.Substring(0, 5);
+            var suffix = hash.Substring(5);
+
+            // Get result from returned range.
+            var results = GetRange(prefix);
+            return results.ContainsKey(suffix) ? results[suffix] : 0;
+        }
+
+        /// <summary>
+        /// Gets the number of times the given password appears according to the service.
+        /// </summary>
+        /// <param name="password">The password to look up.</param>
+        /// <returns></returns>
+        [Obsolete("This method is to be removed in a future version of the API. Use GetNumberOfAppearances instead.")]
+        public int GetNumberOfAppearancesUsingEndpoint(string password)
         {
             try
             {
