dd7718e5466a1853201ddf7d926b900cd233997c
Saul Johnson <saul.a.johnson@gmail.com>
2018-06-24 04:27:19 +0100

    Add frequency extractor

---
+++ PwnedApiWrapper.FrequencyExtractor/FrequencyExtractorWrapper.cs
@@ -0,0 +1,61 @@
+﻿using PwnedApiWrapper.Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PwnedApiWrapper.FrequencyExtractor
+{
+    /// <summary>
+    /// Represents a wrapper around the Pwned Passwords corpus file for extracting frequencies only.
+    /// </summary>
+    class FrequencyExtractorWrapper
+    {
+        /// <summary>
+        /// Gets the path this class is wrapping.
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// Initialises a new instance of a wrapper around the Pwned Passwords corpus file for extracting frequencies only.
+        /// </summary>
+        public FrequencyExtractorWrapper(string path)
+        {
+            Path = path;
+        }
+
+        /// <summary>
+        /// Pulls all frequencies from the file above a limit.
+        /// </summary>
+        /// <param name="limit">The lower limit of frequencies to extract.</param>
+        /// <returns>A dictionary of passwords against counts.</returns>
+        public List<int> Pull(int limit)
+        {
+            // Prepare output dictionary.
+            var output = new List<int>();
+
+            // Read file one line at a time, has to be buffered, file too big otherwise.
+            using (var reader = new BufferedStream(new FileStream(Path, FileMode.Open)))
+            {
+                // Lines are 63 bytes long (including line endings, which are Windows-style).
+                byte[] buffer = new byte[63];
+                while (reader.Read(buffer, 0, buffer.Length) != 0)
+                {
+                    // Decode and split along colon.
+                    var line = Encoding.ASCII.GetString(buffer);
+                    var splitter = line.Trim().Split(':');
+
+                    // Add count to output if above limit.
+                    var x = int.Parse(splitter[1]);
+                    if (x >= limit)
+                    {
+                        output.Add(x);
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ PwnedApiWrapper.FrequencyExtractor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PwnedApiWrapper.Shared;
+
+namespace PwnedApiWrapper.FrequencyExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check number of arguments.
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: App <limit> <pwned_passwords_corpus>");
+                return;
+            }
+
+            // Check file exists.
+            var limit = -1;
+            if (!int.TryParse(args[0], out limit) || !File.Exists(args[1]))
+            {
+                Console.WriteLine("Invalid arguments.");
+                return;
+            }
+
+            // Query file.
+            var service = new FrequencyExtractorWrapper(args[1]);
+            var results = service.Pull(limit);
+
+            // Output goes straight to console.
+            foreach (var entry in results)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
