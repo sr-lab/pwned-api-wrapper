393e609c19d3a26f35ef529619ab29018acfbb72
Saul Johnson <saul.a.johnson@gmail.com>
2018-07-02 15:39:18 +0100

    Imports cleanup in Local library

--- PwnedApiWrapper.Local/PwnedFileWrapper.cs
+++ PwnedApiWrapper.Local/PwnedFileWrapper.cs
@@ -1,10 +1,8 @@
-﻿using PwnedApiWrapper.Shared;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+using PwnedApiWrapper.Shared;
 
 namespace PwnedApiWrapper.Local
 {
